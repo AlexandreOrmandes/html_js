45b9e2932ee5b7f74aff1b5a7f9b825781e74a52
Ale <aleorman@gmail.com>
2023-01-16 08:48:02 -0300

    correção bug

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 const form = document.getElementById('form-login')
-let valorA = (document.getElementById('valor-a'))
-let valorB = (document.getElementById('valor-b'))
-
-
-
+const valorA = ( document.getElementById('valor-a'))
+const valorB = (document.getElementById('valor-b'))
 
 
 form.addEventListener('submit', function(e){
     e.preventDefault()
     const mensagemSucesso = `Formulario Enviado!`
-
-    
-    if(valorB.value>valorA.value){
+    parseInt(valorA.value)
+    parseInt(valorB.value)
+    if  ( valorB.valueAsNumber > valorA.valueAsNumber){
         const containerMensagemSucesso =document.querySelector('.success')
         containerMensagemSucesso.innerHTML = mensagemSucesso
         containerMensagemSucesso.style.display = 'block'
     
     valorA.value = ''
     valorB.value = ''
+    
 
 } else {
     document.querySelector('.error-message').style.display = 'block'
 }
-
-
 })
+ console.log(valorB)
